218d7b6084cab664c2fb218169c82d372aa13093
Mirko Masiero <mirko.masiero@marconirovereto.it>
2018-08-18 18:10:34 +0200

    Rewrite Unit Tests

--- src/test/java/com/example/shoprating/ShopRatingApplicationTests.java
+++ src/test/java/com/example/shoprating/ShopRatingApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.shoprating;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ShopRatingApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
@@ -24,43 +27,45 @@ public class ShopRatingApplicationTests {
 	}
 	
 	@Test
-	public void testPostRating() throws Exception {
+	public void test1_PostRating() throws Exception {
 		RequestBuilder postRequest = MockMvcRequestBuilders.post("/api/ratings/{productId}/{userId}",
-				"b99c88ce-c135-48ac-a756-a2fc3e5e4ab6","33")
-				.content("{\"rate\": 3, \"comment\" : \"Very nice!\" }")
+				"cbdbc72c-977f-450b-9b0e-b6eca9a7f200","33")
+				.content("{\"rate\": 5, \"comment\" : \"Very nice!\" }")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaType.APPLICATION_JSON_UTF8);
 		
 		mockMvc.perform(postRequest)
 			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].rate").value("3"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].comment").value("Very Nice"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].userId").value("33"));
+			.andExpect(MockMvcResultMatchers.jsonPath("$.rate").value(5))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.comment").value("Very nice!"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.userId").value("33"));
 	}
 	
 	@Test
-	public void testGetRating() throws Exception {
+	public void test2_GetRating() throws Exception {
 		RequestBuilder getRequest = MockMvcRequestBuilders.get("/api/ratings/{productId}",
-				"b99c88ce-c135-48ac-a756-a2fc3e5e4ab6")
+				"cbdbc72c-977f-450b-9b0e-b6eca9a7f200")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaType.APPLICATION_JSON_UTF8);
 		
 		mockMvc.perform(getRequest)
 			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].rate").value("3"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].comment").value("Very Nice"));
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].rate").value(5))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].comment").value("Very nice!"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].userId").value("33"));
 	}
 
 	
 	@Test
-	public void testGetPopular() throws Exception {
-		RequestBuilder getRequest = MockMvcRequestBuilders.get("/api/popular")
+	public void test3_GetPopular() throws Exception {
+		RequestBuilder getRequest = MockMvcRequestBuilders.get("/api/ratings/popular")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaType.APPLICATION_JSON_UTF8);
 		
 		mockMvc.perform(getRequest)
 			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Water"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Smartphone Samsung")); //To verify
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Iron steam")) //Due to previous tests
+			.andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Chair"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[1].averageRate").value(5));
 	}
 }
