3786e643e5b21eed92db24858aea829eb6b974f1
Mirko Masiero <mirko.masiero@marconirovereto.it>
2018-08-20 15:42:58 +0200

    Add Hystrix circuit breaker

--- src/main/java/com/example/shoprating/service/ProductService.java
+++ src/main/java/com/example/shoprating/service/ProductService.java
@@ -5,16 +5,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.shoprating.dto.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class ProductService {
 	@Autowired
 	private RestTemplate rt;
 	
+	@HystrixCommand(fallbackMethod="defaultProduct")
 	public Product getProductById(String productId) {
 		Product product = rt.getForObject(
 				"http://shop-catalog/api/products/{productId}", Product.class, productId);
 		
+		return product;
+	}
+	
+	public Product defaultProduct(String productId) {
+		Product product = new Product();
+		product.setId("0000");
+		product.setTitle("Unavaible");
+		product.setAvailability(0);
 		return product;
 	}
 }
